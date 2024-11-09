1b6c4cc189dafd81d8d7c274ab9e52eeb7624692
Steve Fernandes <steve1818fernandes@gmail.com>
2024-11-09 19:08:38 +0530

    [FEAT] Scanner Partially Implemented

--- src/api/controllers/imageController.js
+++ src/api/controllers/imageController.js
@@ -1,20 +1,49 @@
 const Image = require("../models/imageModel");
+const { GoogleGenerativeAI } = require("@google/generative-ai");
+
+let genAIInstance = null;
+let modelInstance = null;
+
+// Function to initialize the GoogleGenerativeAI instance and model if they haven't been created yet
+const initializeGenAI = () => {
+  if (!genAIInstance) {
+    genAIInstance = new GoogleGenerativeAI(process.env.GEMINIAPI);
+  }
+  if (!modelInstance) {
+    modelInstance = genAIInstance.getGenerativeModel({
+      model: "gemini-1.5-flash",
+    });
+  }
+};
 
 // Handle image upload
-const uploadImage = async (req, res) => {
+const uploadImage = async (req, res, next) => {
   try {
-    const newImage = new Image({
-      data: req.file.buffer,
-      contentType: req.file.mimetype,
-      type: req.body.type,
-      userId: req.user._id,
-    });
+    // const newImage = new Image({
+    //   data: req.file.buffer,
+    //   contentType: req.file.mimetype,
+    //   type: req.body.type,
+    //   userId: req.user._id,
+    // });
 
-    await newImage.save();
-    res.status(200).json({ message: "Image uploaded successfully" });
+    const image = {
+      inlineData: {
+        data: req.file.buffer.toString("base64"),
+        mimeType: req.file.mimetype,
+      },
+    };
+
+    initializeGenAI();
+    const response = await modelInstance.generateContent([
+      image,
+      `Provide the electricity bill in the following format JSON only in a normal text prompt not markdown, rememeber that true and false in JSON are lowercase and all the data other than Boolean should have quotes: {"isBillProvided": <Boolean>, "billDetails": { "amount": <Number>, "billDate": <String>, "dueDate": <String>, "units": <Number> } | null}`,
+    ]);
+    return res.status(200).json({
+      message: "Text Fetched Successfully",
+      response: JSON.parse(response.response.text()),
+    });
   } catch (error) {
-    console.error("Error saving image:", error);
-    res.status(500).json({ message: "Error uploading image" });
+    return next(error);
   }
 };
 
