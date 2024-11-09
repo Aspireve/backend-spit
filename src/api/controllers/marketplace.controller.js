819575d5093a409495cc391eabe697321b494ae5
Steve Fernandes <steve1818fernandes@gmail.com>
2024-11-09 20:22:59 +0530

    [FEAT] Completed getting the market

--- src/api/controllers/marketplace.controller.js
+++ src/api/controllers/marketplace.controller.js
@@ -1,4 +1,6 @@
+const Image = require("../models/imageModel");
 const { GoogleGenerativeAI } = require("@google/generative-ai");
+const PossibleBuyer = require("../models/possibleBuyers");
 
 let genAIInstance = null;
 let modelInstance = null;
@@ -15,29 +17,42 @@ const initializeGenAI = () => {
   }
 };
 
-exports.getElectricityBillDetails = async (req, res, next) => {
+// Handle image upload
+exports.isRecycleable = async (req, res, next) => {
   try {
+
+    const image = {
+      inlineData: {
+        data: req.file.buffer.toString("base64"),
+        mimeType: req.file.mimetype,
+      },
+    };
+
     initializeGenAI();
-    const image = "";
     const response = await modelInstance.generateContent([
-      `Provide me the electricity bill details from the image in the following JSON format only, no other format is allowed, if no bill is provided, set the isBillProvided false and putt the next object as null
-        BillDetails =  {
-            isBillProvided: <Boolean>
-            billDetails: {
-                amount: <Number>,
-                billDate: <String>,
-                dueDate: <String>,
-                units: <Number>,
-            } | null
-        }`,
       image,
+      `Provide me data fromthe image in the following format JSON only in a normal text prompt not markdown, rememeber that true and false in JSON are lowercase and all the data other than Boolean should have quotes: {"allItemsSameType": <Boolean>, "itemType": enum("recycleable", "e-waste", "non-recycleable"),"itemDatabase": enum("aluminium", "battery", "books", "brass", "cardboard", "ceiling fan", "copper", "inverter", "iron", "metal e-waste", "motors ", "paper", "plastic bottles", "plastic e-waste", "printer", "steel utensils", null), "itemDescription": <String>}`,
     ]);
     return res.status(200).json({
       message: "Text Fetched Successfully",
-      response: response.response.text(),
+      response: JSON.parse(response.response.text()),
     });
-    // const electricityBillDetails = await ElectricityBillDetails.find();
-    // res.json(electricityBillDetails);
+  } catch (error) {
+    return next(error);
+  }
+};
+
+// Handle image upload
+exports.provideAmount = async (req, res, next) => {
+  try {
+    const {itemDatabase, quantity} = req.query
+
+    const rateDocument = await PossibleBuyer.findOne({object: itemDatabase});
+
+    return res.status(200).json({
+      message: "Rates Fetched Successfully",
+      rate: rateDocument.rate * quantity,
+    });
   } catch (error) {
     return next(error);
   }
--- src/api/routes/v1/marketplace.route.js
+++ src/api/routes/v1/marketplace.route.js
@@ -1,11 +1,25 @@
 const express = require("express");
+const multer = require("multer");
 const marketController = require("../../controllers/marketplace.controller");
 const { authorize, LOGGED_USER } = require("../../middlewares/auth");
 
 const router = express.Router();
 
+const storage = multer.memoryStorage();
+const upload = multer({ storage });
 router
-  .route("/electricityBill")
-  .post(authorize(LOGGED_USER), marketController.getElectricityBillDetails);
+  .route("/isRecycleable")
+  .post(
+    authorize(LOGGED_USER),
+    upload.single("image"),
+    marketController.isRecycleable
+  );
+
+router
+  .route("/provideAmount")
+  .get(
+    authorize(LOGGED_USER),
+    marketController.provideAmount
+  );
 
 module.exports = router;
