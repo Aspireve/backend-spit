6192e6db43ad6ab1745ef8e494af7ec96e6985b1
Steve Fernandes <steve1818fernandes@gmail.com>
2024-11-10 08:15:27 +0530

    [BUG] Fixed Marketplace Not FOund Error

--- src/api/controllers/marketplace.controller.js
+++ src/api/controllers/marketplace.controller.js
@@ -1,4 +1,4 @@
-const Image = require("../models/imageModel");
+// const Image = require("../models/imageModel");
 const { GoogleGenerativeAI } = require("@google/generative-ai");
 const PossibleBuyer = require("../models/possibleBuyers");
 
@@ -33,9 +33,14 @@ exports.isRecycleable = async (req, res, next) => {
       image,
       `Provide me data fromthe image in the following format JSON only in a normal text prompt not markdown, rememeber that true and false in JSON are lowercase and all the data other than Boolean should have quotes: {"allItemsSameType": <Boolean>, "itemType": enum("recycleable", "e-waste", "non-recycleable"),"itemDatabase": enum("aluminium", "battery", "books", "brass", "cardboard", "ceiling fan", "copper", "inverter", "iron", "metal e-waste", "motors ", "paper", "plastic bottles", "plastic e-waste", "printer", "steel utensils", null), "itemDescription": <String>}`,
     ]);
+
+    const data = JSON.parse(response.response.text())
+    const rateDocument = await PossibleBuyer.findOne({object: data?.itemDatabase});
+    if(rateDocument)
+      data.rate = rateDocument.rate
     return res.status(200).json({
       message: "Text Fetched Successfully",
-      response: JSON.parse(response.response.text()),
+      response: data,
     });
   } catch (error) {
     return next(error);
